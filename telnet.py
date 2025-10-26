15c7cd641b32c328fd018099e38769d22ce38e9b
Mary McDonald <mcdonald.e.mary@gmail.com>
2025-10-25 21:57:52 -0400

    separate host from path

--- telnet.py
+++ telnet.py
@@ -2,3 +2,8 @@ class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
         assert self.scheme == "http"
+
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
